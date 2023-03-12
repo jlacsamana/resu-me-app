f2132a676822cdccc8a9e2cc2a7d5dee7830a084
Jacob <jacoblacsamana0@gmail.com>
2023-03-11 17:56:46 -0800

    a

--- src/QuickApplier.js
+++
@@ -1,9 +0,0 @@
-import PageWrapper from '../components/PageWrapper';
-
-export function QuickApplier() {
-    return (
-        <PageWrapper>
-            <p>Send nudes</p>
-        </PageWrapper>
-    );
-}
