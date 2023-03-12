d86e58837e05c30fdaa5fe3363fc94f94d98ec68
Norman Vo <92652998+hquanvo@users.noreply.github.com>
2023-03-11 18:27:24 -0800

    Added profile page to side bar

---
+++ src/pages/User.js
@@ -0,0 +1,37 @@
+import PageWrapper from '../components/PageWrapper';
+
+
+export function User() {
+  function editProfile() {
+    document.getElementById("username").disabled = false;
+    document.getElementById("email").disabled = false;
+    document.getElementById("age").disabled = false;
+    document.getElementById("gender").disabled = false;
+    document.getElementById("desc").readOnly = false;
+    document.getElementById("saveButton").disabled = false;
+  }
+
+  function saveProfile() {
+    document.getElementById("username").disabled = true;
+    document.getElementById("email").disabled = true;
+    document.getElementById("age").disabled = true;
+    document.getElementById("gender").disabled = true;
+    document.getElementById("desc").readOnly = true;
+    document.getElementById("saveButton").disabled = true;
+  }
+
+  return (
+    <PageWrapper>
+        <h2>User Profile</h2>
+        Name: <input type = "text" id = "username" disabled = "true" /><br></br>
+        Email: <input type = "text" id = "email" disabled = "true"/><br></br>
+        Age: <input type = "text" id = "age" disabled = "true"/><br></br>
+        Gender: <input type = "text" id = "gender" disabled = "true"/><br></br>
+        Description: <textarea type = "text" id = "desc" rows = "5" cols = "33" readOnly = "true"/><br></br>
+        <button onClick ={editProfile}> Edit profile </button>
+        <button onClick ={saveProfile} id = "saveButton" disabled> Save </button>
+    </PageWrapper>
+  );
+
+
+}
